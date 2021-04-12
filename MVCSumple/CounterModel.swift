fc039f6dfc5d1533b3f15a64389f5059e8551e8e
TazawaAyumu <tazawaayumu@gmail.com>
2021-04-12 22:08:13 +0900

    Inital Commit

---
+++ MVCSumple/CounterModel.swift
@@ -0,0 +1,15 @@
+//
+//  CounterModel.swift
+//  MVCSumple
+//
+//  Created by 田澤歩 on 2021/04/12.
+//
+
+import Foundation
+
+class CounterClass {
+    
+    static let notificationName = "CounterModelChanged"
+    
+    let notificationCountern = NotificationCenter()
+}
